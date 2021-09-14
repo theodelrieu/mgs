ca423d8aa31fa7992c4cc83ed42ec312313d4cd2
Théo Delrieu <theo.delrieu@tanker.io>
2021-09-14 10:29:34 +0200

    config: fix macros MGS_HAS_CXX, remove unused one

--- config/include/mgs/config.hpp
+++ config/include/mgs/config.hpp
@@ -1,22 +1,17 @@
 #pragma once
 
 #ifdef _MSC_VER
-#  if _MSVC_LANG >= 201402L
-#    define MGS_HAS_CPP14
-#    if _MSVC_LANG >= 201703L
-#      define MGS_HAS_CPP17
-#      define MGS_HAS_CPP20
-#    endif
+#  if _MSVC_LANG > 201703L
+#    define MGS_HAS_CPP20
+#    define MGS_HAS_CPP17
+#  elif _MSVC_LANG > 201402L
+#    define MGS_HAS_CPP17
 #  endif
 #else
 #  if __cplusplus >= 201709L
 #    define MGS_HAS_CPP20
 #    define MGS_HAS_CPP17
-#    define MGS_HAS_CPP14
 #  elif __cplusplus >= 201703L
 #    define MGS_HAS_CPP17
-#    define MGS_HAS_CPP14
-#  elif __cplusplus >= 201402L
-#    define MGS_HAS_CPP14
 #  endif
 #endif
